cfc6fc976bf2137e75da6cc59ef92c67f854d901
servletdemo <navyasrikuppala123@gmail.com>
2026-09-22 22:29:32 +0530

    init comit

---
+++ src/main/java/org/example/githubintillijsession/GithubIntillijSessionApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubintillijsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIntillijSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIntillijSessionApplication.class, args);
+    }
+
+}
